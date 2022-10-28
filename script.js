a64e2018c8c3d6c5aea2b0947ae4407f84c6a4d0
anderson <anderson.ap133@gmail.com>
2022-10-28 19:04:18 -0300

    checkpoint

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('test');
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
+var start = function() {
+    var audio = document.getElementById('audio');
+    var ctx = new AudioContext();
+    var analyser = ctx.createAnalyser();
+    var audioSrc = ctx.createMediaElementSource(audio);
+    audioSrc.connect(analyser);
+    analyser.connect(ctx.destination);
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+    var canvas = document.getElementById('canvas'),
+        cwidth = canvas.width,
+        cheight = canvas.height - 2,
+        meterWidth = 10 
+}
