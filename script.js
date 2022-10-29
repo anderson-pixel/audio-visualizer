265ff75d55990256c3695e03561e027a529632c7
anderson <anderson.ap133@gmail.com>
2022-10-29 05:01:49 -0300

    checkpoint

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ var start = function() {
     function renderFrame() {
         var array = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var step = Math.round(array.length / meterNum); 
         ctx.clearRect(0, 0, cwidth, cheight);
         for (var i = 0; i < meterNum; i++) {
             var value = array[i * step];
@@ -33,8 +32,6 @@ var start = function() {
                 capYPositionArray.push(value);
             };
             ctx.fillStyle = capStyle;
-            ctx.fillStyle = gradient; 
-            ctx.fillRect(i * 12 , cheight - value + capHeight, meterWidth, cheight); //the meter
         }
         requestAnimationFrame(renderFrame);
     }
