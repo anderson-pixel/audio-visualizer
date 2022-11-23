58dc8b0b2f14a95e72a651f8fdaaafcd02e64fa5
anderson <anderson.ap133@gmail.com>
2022-11-23 08:50:42 -0300

    checkpoint

--- script.js
+++ script.js
@@ -5,24 +5,21 @@ var start = function() {
     var ctx = new AudioContext();
     var analyser = ctx.createAnalyser();
     var audioSrc = ctx.createMediaElementSource(audio);
-    // we have to connect the MediaElementSource with the analyser 
     audioSrc.connect(analyser);
     analyser.connect(ctx.destination);
-    // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
-    // analyser.fftSize = 64;
-    // frequencyBinCount tells you how many values you'll receive from the analyser
     var frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-    // we're ready to receive some data!
+
+   
     var canvas = document.getElementById('canvas'),
         cwidth = canvas.width,
         cheight = canvas.height - 2,
-        meterWidth = 10, //width of the meters in the spectrum
-        gap = 2, //gap between meters
+        meterWidth = 10, 
+        gap = 2, 
         capHeight = 2,
         capStyle = 'white',
-        meterNum = 1000 / (10 + 1) , //count of the meters
-        capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+        meterNum = 1000 / (10 + 1) , 
+        capYPositionArray = []; 
     ctx = canvas.getContext('2d'),
     gradient = ctx.createLinearGradient(450, 200, 210, 200);
     gradient.addColorStop(1, '#f983ff');
@@ -32,7 +29,7 @@ var start = function() {
     function renderFrame() {
         var array = new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
-        var step = Math.round(array.length / meterNum); //sample limited data from the total array
+        var step = Math.round(array.length / meterNum); 
        
         ctx.clearRect(0, 0, cwidth, cheight);
         for (var i = 0; i < meterNum; i++) {
@@ -41,10 +38,10 @@ var start = function() {
                 capYPositionArray.push(value);
             };
             ctx.fillStyle = capStyle;
-            //draw the cap, with transition effect
+           
           
-            ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+            ctx.fillStyle = gradient; 
+            ctx.fillRect(i * 12  , cheight - value + capHeight, meterWidth, cheight); 
         }
         requestAnimationFrame(renderFrame);
     }
